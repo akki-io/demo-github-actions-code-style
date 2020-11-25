dd2333e921a03226af081e1f1e52621766b5d8fe
akki-io <akki-io@users.noreply.github.com>
2020-11-25 05:23:17 +0000

    Apply prettier changes

--- sample.vue
+++ sample.vue
@@ -56,13 +56,16 @@
                         >vue-devtools</a
                     >
                 </li>
-              <li> <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
-              </li>
-              <li>
-
-                <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">
-                  awesome-vue</a>
-              </li>
+                <li>
+                    <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+                        >vue-loader</a
+                    >
+                </li>
+                <li>
+                    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">
+                        awesome-vue</a
+                    >
+                </li>
             </ul>
         </div>
     </div>
